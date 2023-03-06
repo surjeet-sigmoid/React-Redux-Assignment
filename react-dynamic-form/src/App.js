13225a32880adba61e1f0443b1ce73a37d1bfd0a
surjeet-sigmoid <surjeetkv@sigmoidanalytics.com>
2023-03-07 01:28:05 +0530

    react-rdux

--- react-dynamic-form/src/App.js
+++ react-dynamic-form/src/App.js
@@ -1,8 +1,27 @@
-// import "./App.css";
 import Form from "./components/Form";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1f2937",
+      white: "#ffffff",
+    },
+    text: {
+      primary: "#173A5E",
+      secondary: "#46505A",
+    },
+  },
+});
 
 function App() {
-  return <Form />;
+  return (
+    <CssBaseline>
+      <ThemeProvider theme={theme}>
+        <Form />
+      </ThemeProvider>
+    </CssBaseline>
+  );
 }
 
 export default App;
--- react-dynamic-form/src/store/formSlice.js
+++ react-dynamic-form/src/store/formSlice.js
@@ -1,20 +1,24 @@
 import axios from "axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { keyframes } from "@emotion/react";
 
 const initialState = {
   loading: true,
   inputs: [],
   error: "",
   formInputs: {},
+  disable: false,
 };
 
-export const fetchInputs = createAsyncThunk("form/fetchInputs", () => {
-  let url = "https://run.mocky.io/v3/a55c4590-c635-49af-a01f-7ee2e6a85669";
-  url = "https://run.mocky.io/v3/7ec8da10-b0ee-4016-86a0-100925968a0c";
+export const fetchInputs = createAsyncThunk(
+  "form/fetchInputs",
+  (state, action) => {
+    let url = "https://run.mocky.io/v3/a55c4590-c635-49af-a01f-7ee2e6a85669";
+    url = "https://run.mocky.io/v3/7ec8da10-b0ee-4016-86a0-100925968a0c";
 
-  return axios.get(url).then((response) => response.data);
-});
+    let data = axios.get(url).then((response) => response.data);
+    return data;
+  }
+);
 
 const formSlice = createSlice({
   name: "form",
@@ -23,8 +27,19 @@ const formSlice = createSlice({
     handleInputChange: (state, action) => {
       const { key, val } = action.payload;
       state.formInputs[key] = val;
-      // console.log(key, val);
-      console.log(state.inputs);
+      state.disable = state.formInputs[key] ? false : true;
+    },
+    handleCheckBoxInputChange: (state, action) => {
+      const { key, val, checked } = action.payload;
+      if (checked) {
+        (state.formInputs[key] ||= []).push(val);
+      } else {
+        const index = (state.formInputs[key] ||= []).indexOf(val);
+        if (index > -1) {
+          state.formInputs[key].splice(index, 1);
+        }
+      }
+      state.disable = state.formInputs[key].length ? false : true;
     },
   },
   extraReducers: (builder) => {
@@ -35,6 +50,10 @@ const formSlice = createSlice({
       state.loading = false;
       state.inputs = action.payload;
       state.error = "";
+      state.inputs.forEach((val, idx) => {
+        state.formInputs[val.id] = val.defaultVal;
+      });
+      // console.log(state.inputs);
     });
     builder.addCase(fetchInputs.rejected, (state, action) => {
       state.loading = false;
@@ -44,6 +63,7 @@ const formSlice = createSlice({
   },
 });
 
-export const { handleInputChange } = formSlice.actions;
+export const { handleInputChange, handleCheckBoxInputChange } =
+  formSlice.actions;
 
 export default formSlice.reducer;
